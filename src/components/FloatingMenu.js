10097e13b0d5b9b5e07187395124cf5b320974a7
mckostick <mckostick@gmail.com>
2024-06-24 17:50:03 -0400

    lk

--- src/components/FloatingMenu.js
+++ src/components/FloatingMenu.js
@@ -8,7 +8,7 @@ const FloatingMenu = () => {
       <ul>
         <li><a href="#media">Media</a></li>
         <li><a href="#locations">Locations</a></li>
-        <li><a href="#events">Events</a></li>
+        <li><a href="#events">fuck</a></li>
         <li><a href="#shop">Shop</a></li>
         <li><a href="#merch">Merch</a></li>
         <li><a href="#rewards">Rewards</a></li>
