0a7550379bcfb267d80cb6c52f3915a13a8362b6
mckostick <mckostick@gmail.com>
2024-06-25 11:31:47 -0400

    ok

--- src/components/FloatingMenu.js
+++ src/components/FloatingMenu.js
@@ -5,16 +5,7 @@ import './FloatingMenu.css';
 const FloatingMenu = () => {
   return (
     <div className="floating-menu">
-      <ul>
-        <li><a href="#media">Media</a></li>
-        <li><a href="#locations">Locations</a></li>
-        <li><a href="#events">fuck</a></li>
-        <li><a href="#shop">Shop</a></li>
-        <li><a href="#merch">Merch</a></li>
-        <li><a href="#rewards">Rewards</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      <p className="swipe">Swipe up to continue</p>
+      <p className="swipe">Under Construction</p>
     </div>
   );
 };
