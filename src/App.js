679939d2d2aa4a4310f7a445979666aa2c71bfcc
mckostick <mckostick@gmail.com>
2024-06-25 15:05:36 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// src/BackgroundVideo.js
+import React from 'react';
+import './BackgroundVideo.css';
 
-function App() {
+const BackgroundVideo = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background-video">
+      <video autoPlay loop muted>
+        <source src={`${process.env.PUBLIC_URL}/background.mp4`} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
-}
+};
 
-export default App;
+export default BackgroundVideo;
+
