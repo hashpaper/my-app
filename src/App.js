0e7e2bc37625c00aa17282b77d9e5798c71283c0
mckostick <mckostick@gmail.com>
2024-06-25 14:50:46 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,2 +1,19 @@
+// src/App.js
+import React from 'react';
+import BackgroundVideo from './Backgroundvideo';
+import './App.css';
 
+const App = () => {
+  return (
+    <div className="App">
+      <BackgroundVideo />
+      <div className="content">
+        <h1>Welcome to My Website</h1>
+        <p>This is an example of a website with a background video.</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
 
--- src/Backgroundvideo.js
+++ src/Backgroundvideo.js
@@ -15,23 +15,5 @@ const BackgroundVideo = () => {
 
 export default BackgroundVideo;
 
-// src/App.js
-import React from 'react';
-import BackgroundVideo from './Backgroundvideo';
-import './App.css';
-
-const App = () => {
-  return (
-    <div className="App">
-      <BackgroundVideo />
-      <div className="content">
-        <h1>Welcome to My Website</h1>
-        <p>This is an example of a website with a background video.</p>
-      </div>
-    </div>
-  );
-};
-
-export default App;
 
 
