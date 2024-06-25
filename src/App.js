92c7079f3763a3554d50a4c5a6ae681090add146
mckostick <mckostick@gmail.com>
2024-06-25 11:54:49 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -12,3 +12,39 @@ function App() {
 }
 
 export default App;
+
+// src/BackgroundVideo.js
+import React from 'react';
+import './BackgroundVideo.css';
+
+const BackgroundVideo = () => {
+  return (
+    <div className="background-video">
+      <video autoPlay loop muted>
+        <source src={`${process.env.PUBLIC_URL}/background.mp4`} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
+
+// src/App.js
+import React from 'react';
+import BackgroundVideo from './BackgroundVideo';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BackgroundVideo />
+      <div className="content">
+        <h1>Welcome to My Website</h1>
+        <p>This is an example of a website with a background video.</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
