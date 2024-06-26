4c4823198ee30079186c7aa5019d22217b9325ba
mckostick <mckostick@gmail.com>
2024-06-26 13:41:41 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,4 @@
 // src/App.js
-import React from 'react';
-import BackgroundVideo from './BackgroundVideo';
-import './App.css';
-
-const App = () => {
-  return (
-    <div className="App">
-      <BackgroundVideo />
-      <div className="content">
-        <h1>Welcome to My Website</h1>
-        <p>This is an example of a website with a background video.</p>
-      </div>
-    </div>
-  );
-};
 
 import React from 'react';
 import './App.css';
