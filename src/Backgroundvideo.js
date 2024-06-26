40765419b5091b0926b9ca3cd56f75bd3b36ab5e
mckostick <mckostick@gmail.com>
2024-06-26 13:49:34 -0400

    ok

--- src/Backgroundvideo.js
+++ src/Backgroundvideo.js
@@ -4,7 +4,7 @@ import './BackgroundVideo.css'; // We'll add some styling here
 const BackgroundVideo = () => {
   return (
     <div className="background-video">
-      <video autoPlay loop muted>
+      <video autoPlay loop>
         <source src={`${process.env.PUBLIC_URL}/background.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
